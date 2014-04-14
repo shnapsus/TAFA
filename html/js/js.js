dfcf486e89b687c2b04f71da7ea432628a9f4cd1
shnapsus <shnapsus@gmail.com>
2014-04-14 15:20:01 +0300

    qqq

--- html/js/js.js
+++ html/js/js.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
+	$('.header__nav__ul__li__a').click(function () {
+		$('.content').css('display', 'none');
+		$('.content'+ $(this).attr('href')).css('display', 'block');
+
+		return false;
+	});
+
+	if($('.select').length>0){
+		var params = {
+			changedEl: ".select",
+			visRows: 5,
+			scrollArrows: true
+		}
+		cuSel(params);
+	}
+
 
 
 	jQuery('input[placeholder], textarea[placeholder]').placeholder();
